1622c7dbe00bf300276dde1d525a88d0bb0dad8d
Andrew Lunde <andrew.lunde@sap.com>
2022-09-13 16:27:54 -0400

    local dev

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # crypto-rates-service
+
+```
+make docker-push
+make helm-deploy
+
+make docker-push ; make helm-deploy
+
+make helm-undeploy
+```
--- srv/server.js
+++ srv/server.js
@@ -2,37 +2,41 @@ const express = require('express');
 const app = express();
 const xsenv = require('@sap/xsenv');
 xsenv.loadEnv();
-const services = xsenv.getServices({
-    uaa: { label: 'xsuaa' }
-});
 
+// x_Disable
+// const services = xsenv.getServices({
+//     uaa: { label: 'xsuaa' }
+// });
 
-const xssec = require('@sap/xssec');
-const passport = require('passport');
-passport.use('JWT', new xssec.JWTStrategy(services.uaa));
-app.use(passport.initialize());
-app.use(passport.authenticate('JWT', {
-    session: false
-}));
+// x_Disable
+// const xssec = require('@sap/xssec');
+// const passport = require('passport');
+// passport.use('JWT', new xssec.JWTStrategy(services.uaa));
+// app.use(passport.initialize());
+// app.use(passport.authenticate('JWT', {
+//     session: false
+// }));
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/srv', function (req, res) {
-    if (req.authInfo.checkScope('$XSAPPNAME.User')) {
+    // x_Disable
+    // if (req.authInfo.checkScope('$XSAPPNAME.User')) {
         res.status(200).send('crypto-rates-service');
-    } else {
-        res.status(403).send('Forbidden');
-    }
+    // } else {
+    //     res.status(403).send('Forbidden');
+    // }
 });
 
 app.get('/srv/user', function (req, res) {
-    if (req.authInfo.checkScope('$XSAPPNAME.User')) {
+    // x_Disable
+    // if (req.authInfo.checkScope('$XSAPPNAME.User')) {
         res.status(200).json(req.user);
-    } else {
-        res.status(403).send('Forbidden');
-    }
+    // } else {
+        // res.status(403).send('Forbidden');
+    // }
 });
 
 
