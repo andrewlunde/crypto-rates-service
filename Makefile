df3daa7c11438fec2fb54ef09d41c370d6ee5a10
Andrew Lunde <andrew.lunde@sap.com>
2022-09-14 16:18:22 -0400

    npm wrong arch

--- Makefile
+++ Makefile
@@ -12,5 +12,9 @@ helm-deploy:
 	helm upgrade -n dev -i crypto-rates-service-app helm/crypto-rates-service-app --install
 
 helm-undeploy:
+#	mv app/default-env.json default-env-app.json
+#	mv srv/default-env.json default-env-srv.json
 	helm uninstall -n dev crypto-rates-service-app
 	helm uninstall -n dev crypto-rates-service-srv
+#	mv default-env-srv.json srv/default-env.json
+#	mv default-env-app.json app/default-env.json
--- srv/server.js
+++ srv/server.js
@@ -8,6 +8,11 @@ xsenv.loadEnv();
 //     uaa: { label: 'xsuaa' }
 // });
 
+const sqlite3 = require('sqlite3').verbose();
+// const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database('data/crypto-rates.db');
+db.exec("PRAGMA synchronous = 2");  // Force write-through to file system
+
 // x_Disable
 // const xssec = require('@sap/xssec');
 // const passport = require('passport');
@@ -39,9 +44,81 @@ app.get('/srv/user', function (req, res) {
     // }
 });
 
+app.get('/adm', function (req, res) {
+    // x_Disable
+    // if (req.authInfo.checkScope('$XSAPPNAME.Admin')) {
+        res.status(200).send('crypto-rates-service');
+    // } else {
+    //     res.status(403).send('Forbidden');
+    // }
+});
 
+app.get('/adm/user', function (req, res) {
+    // x_Disable
+    // if (req.authInfo.checkScope('$XSAPPNAME.Admin')) {
+        res.status(200).json(req.user);
+    // } else {
+        // res.status(403).send('Forbidden');
+    // }
+});
 
+function do_DB_Init() {
+    db.serialize(() => {
+        db.run("CREATE TABLE IF NOT EXISTS lorem (info TEXT)");
+    });
+    
+    //db.close();
+}
 
+app.get('/adm/db_init', function (req, res) {
+    // x_Disable
+    // if (req.authInfo.checkScope('$XSAPPNAME.Admin')) {
+        do_DB_Init();
+        res.status(200).send('db_init');
+    // } else {
+        // res.status(403).send('Forbidden');
+    // }
+});
+
+function do_DB_Fill() {
+    db.serialize(() => {
+        const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+        for (let i = 0; i < 10; i++) {
+            stmt.run("Ipsum " + i);
+        }
+        stmt.finalize();
+    });
+    
+    //db.close();
+}
+
+app.get('/adm/db_fill', function (req, res) {
+    // x_Disable
+    // if (req.authInfo.checkScope('$XSAPPNAME.Admin')) {
+        do_DB_Fill();
+        res.status(200).send('db_fill');
+    // } else {
+        // res.status(403).send('Forbidden');
+    // }
+});
+
+function do_DB_Dump() {
+    db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+        console.log(row.id + ": " + row.info);
+    });
+    
+    // db.close();
+}
+
+app.get('/adm/db_dump', function (req, res) {
+    // x_Disable
+    // if (req.authInfo.checkScope('$XSAPPNAME.Admin')) {
+        do_DB_Dump();
+        res.status(200).send('db_dump');
+    // } else {
+        // res.status(403).send('Forbidden');
+    // }
+});
 
 
 
